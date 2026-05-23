64ffe08bda856d73f47c29872a38e243bc9cf771
JuanLeiteDev <juanleite048@gmail.com>
2026-05-23 01:08:32 +0100

    Criação do backend em python sendo implementada

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 from flask import Flask, request, render_template
+from datetime import datetime
+
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    tarefas = carregar_tarefas()
+    return render_template(
+        "listar.html", 
+        tarefas=tarefas
+    )
+
+@app.route("/criar")
+def criar():
+    return render_template("criar.html")
+
+@app.route("/listar")
+def listar():
+    tarefas = carregar_tarefas()
+    return render_template("listar.html", tarefas=tarefas)
+
+def carregar_tarefas():
+    caminho = "tarefas.json"
+    with open(caminho, "r") as file:
+        tarefas = json.load(file)
+        return tarefas
+    
 
 if __name__ == "__main__":
     app.run()
