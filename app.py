7dfd423e3aba9fa9631c1ab78bb2be4ac989ca6b
JuanLeiteDev <juanleite048@gmail.com>
2026-05-24 02:00:46 +0100

    Todas as rotas e validações criadas

--- app.py
+++ app.py
@@ -10,9 +10,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     tarefas = carregar_tarefas()
+    tarefas_pendentes = [tarefa for tarefa in tarefas["tarefas"] if tarefa["estado"] == ESTADOS[0]]
+    tarefas_atrasadas = [tarefa for tarefa in tarefas["tarefas"] if tarefa["estado"] == ESTADOS[1]]
+    tarefas_concluidas = [tarefa for tarefa in tarefas["tarefas"] if tarefa["estado"] == ESTADOS[2]]
     return render_template(
         "listar.html", 
-        tarefas=tarefas
+        tarefas_pendentes=tarefas_pendentes,
+        tarefas_atrasadas=tarefas_atrasadas,
+        tarefas_concluidas=tarefas_concluidas
     )
 
 @app.route("/criar", methods=["GET", "POST"])
@@ -30,6 +35,7 @@ def criar():
     if len(descricao) > 200: erros["descricao"] = "O tamanho máximo da descrição é de 200 caracteres."
 
     categorias = request.form.get('cat', "").strip()
+    if(len(categorias) > 30): erros["categoria"] = "A categoria deve ter no máximo 30 caracteres."
 
     estado = request.form.get('status', "").strip()
     if estado not in ESTADOS: erros["estado"] = f"O estados disponíveis são: {ESTADOS[0]}, {ESTADOS[1]} e {ESTADOS[2]}"
@@ -60,30 +66,55 @@ def criar():
         "estado": estado,
     }
 
-    adicionar_tarefa(nova_tarefa)
+    if not adicionar_tarefa(nova_tarefa):
+        return render_template(
+            "criar.html",
+            erros="Erro ao guardar tarefa no arquivo JSON",
+            dados=request.form
+        ), 400
     return redirect(url_for('home'))
 
 @app.route("/listar")
 def listar():
-    tarefas = carregar_tarefas()
-    return render_template("listar.html", tarefas=tarefas)
+    return redirect(url_for('home'))
+
+@app.route("/apagar/<id_tarefa>", methods=["POST"])
+def apagar(id_tarefa: int):
+    tarefas_json = carregar_tarefas()
+    tarefas: list[dict] = tarefas_json["tarefas"]
+
+    if not any(int(tarefa["id"]) == int(id_tarefa) for tarefa in tarefas): return {"sucesso": False, "mensagem": "Tarefa não existe."}
+
+    tarefas_json["tarefas"] = [tarefa for tarefa in tarefas if int(tarefa["id"]) != int(id_tarefa)]
+    if salvar_tarefas(tarefas_json): return {"sucesso": True, "mensagem": "Tarefa apagada com sucesso!"}
+    else: return {"sucesso": False, "mensagem": "Erro ao tentar apagar tarefa do banco."}
 
 def carregar_tarefas() -> dict[list]:
-    caminho = "tarefas.json"
-    with open(caminho, "r") as file:
-        tarefas = json.load(file)
-        return tarefas
+    try:
+        with open("tarefas.json", "r", encoding="utf-8") as file:
+            return json.load(file)
+    except Exception:
+        return {
+            "ultimo-id-usado": 0,
+            "categorias": [],
+            "tarefas": []
+        }
+    
+def salvar_tarefas(tarefas):
+    try:
+        with open("tarefas.json", "w") as file:
+            json.dump(tarefas, file, indent=4, ensure_ascii=False)
+            return True
+    except Exception:
+        return False
     
 def adicionar_tarefa(nova: dict):
     tarefas = carregar_tarefas()
 
-    match nova['estado']:
-        case "Pendente":
-            tarefas['pendentes'].append(nova)
-        case "Atrasada":
-            tarefas['atrasadas'].append(nova)
-        case "Concluída":
-            tarefas['concluidas'].append(nova)
+    nova["id"] = tarefas["ultimo-id-usado"] + 1
+    tarefas["ultimo-id-usado"] += 1
+
+    tarefas["tarefas"].append(nova)
 
     try:
         categoria = nova.get("categoria", "")
@@ -92,8 +123,7 @@ def adicionar_tarefa(nova: dict):
     except Exception:
         pass
 
-    with open("tarefas.json", "w") as file:
-        json.dump(tarefas, file, indent=4, ensure_ascii=False)
+    return salvar_tarefas(tarefas)
 
 if __name__ == "__main__":
     app.run()
